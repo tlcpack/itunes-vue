f5477e18016ef3a12c503225a5e07d13bbc1bd1d
Taylor Cooke <taylorcooke@gmail.com>
2020-09-04 08:39:25 -0400

    updated app and installed vuetify

--- itunes-search/src/App.vue
+++ itunes-search/src/App.vue
@@ -1,28 +1,24 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-content>
+      <Search />
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Search from "./components/Search.vue";
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    Search,
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.application {
+  background-color: #a4508b !important;
+  background-image: linear-gradient(326deg, #a4508b 0%, #5f0a87 74%) !important;
 }
 </style>
--- itunes-search/src/main.js
+++ itunes-search/src/main.js
@@ -1,8 +1,11 @@
+import '@babel/polyfill'
 import Vue from 'vue'
+import './plugins/vuetify'
 import App from './App.vue'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  render: h => h(App)
 }).$mount('#app')
+
