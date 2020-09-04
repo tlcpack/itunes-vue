969900c74365a27c43b2072474669082982bc2de
Taylor Cooke <taylorcooke@gmail.com>
2020-09-04 10:09:39 -0400

    removing vuetify

--- itunes-search/src/main.js
+++ itunes-search/src/main.js
@@ -1,10 +1,8 @@
-import '@babel/polyfill'
 import Vue from 'vue'
-import './plugins/vuetify'
 import App from './App.vue'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App)
+  render: h => h(App),
 }).$mount('#app')
